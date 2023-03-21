5e2159020f473619d1bb2c5ec2ea7a57428a1d74
DiegoNoe <diegonoe_vargas@hotmail.com>
2023-03-21 12:10:09 -0600

    primer commit

---
+++ pos/js/index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBhhniDRtbUoE1SGVCTZzVUPTbd5k-mfnI",
+    authDomain: "cremeria-ordena.firebaseapp.com",
+    databaseURL: "https://cremeria-ordena-default-rtdb.firebaseio.com",
+    projectId: "cremeria-ordena",
+    storageBucket: "cremeria-ordena.appspot.com",
+    messagingSenderId: "862495808627",
+    appId: "1:862495808627:web:09d8f34bdb206056f1de06"
+};
